c9cda6d7f15fb7e9bf879287d2e347be3b2303e9
Justrooo <87012050+Justrooo@users.noreply.github.com>
2023-05-16 21:28:36 +0200

    Dodano smieszna funckje
    d

---
+++ smiesznarzecz.js
@@ -0,0 +1,12 @@
+function wypisz()
+{
+    window.alert(`
+    W ułamku sekund strzał
+    Kolejny terro padł
+    W mych dłoniach Desert Strike
+    Wbijam na Bombsite A!
+    W ułamku sekund strzał
+    Kolejny terro padł
+    W mych dłoniach Desert Strike
+    Wbijam na Bombsite A!`)
+}
